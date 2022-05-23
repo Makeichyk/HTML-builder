f6381998f76f605703d9c41355b6fcc08eb10038
Makeichyk <jaroslau1@gmail.com>
2022-05-23 22:56:47 +0400

    feat: complite task №4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 
+const sourcePath = path.join(__dirname, "files");
+const copyPath = path.join(__dirname, "files-copy");
+
+async function makeCopy(src, copy) {
+  try {
+    await fs.promises.mkdir(copy, { recursive: true });
+    
+    let files = await fs.promises.readdir(src, { withFileTypes: true });
+
+    files.forEach((item) => {
+      let fileCopy = path.join(copy, item.name);
+      let fileSrc = path.join(src, item.name);
+
+      if (item.isDirectory()) {
+        makeCopy(fileSrc, fileCopy);
+      } else {
+        fs.promises.copyFile(fileSrc, fileCopy);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+makeCopy(sourcePath, copyPath);
